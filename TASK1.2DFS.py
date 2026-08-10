10e4256fae6b53bb3b911095a7a6027fc842faea
Chaitanya Raju <vchaitanyaraju9@gmail.com>
2026-08-10 09:17:24 +0530

    Add files via upload

---
+++ TASK1.2DFS.py
@@ -0,0 +1,23 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F', 'G'],
+    'D': [],
+    'E': [],
+    'F': [],
+    'G': []
+}
+
+visited = []
+
+def dfs(node):
+    if node not in visited:
+        print(node, end=" ")
+        visited.append(node)
+
+        for neighbour in graph[node]:
+            dfs(neighbour)
+
+start = input("Enter starting node: ")
+print("DFS Traversal:")
+dfs(start)
